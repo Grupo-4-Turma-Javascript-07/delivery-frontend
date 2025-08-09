6098c4d5f84a49374568369e3dafa093557fa77e
Melissa <melissasilva245@gmail.com>
2025-08-09 13:33:12 -0300

    ajustes na home e estilização

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ function Home() {
         <>
             <div id='home' className=" min-h-screen bg-background flex items-center justify-center scroll-mt-20">
                 <div className='container grid grid-cols-2  text-nav-footer'>
-                  <div className="flex justify-center">
+                <div className="flex justify-center">
                         <img
                             src="https://alegrafoods.com.br/wp-content/uploads/2023/06/composicao-kit-feijoada-scaled.jpg"
                             alt="Imagem Página Home"
@@ -14,14 +14,14 @@ function Home() {
                         <h2 className='text-4xl font-bold text-green-400 mb-1'>
                             TemperoBR
                         </h2>
-                        <h3 className=" text-3xl font-light text-green-300 mb-4">
-                          Sabores do Brasil
+                        <h3 className=" text-3xl font-light text-green-300 mb-4 relative">
+                        Sabores do <span className="text-green-400 relative z-10 px-2 text-shadow-lg/15 font-semibold">Brasil<span className="absolute left-0 bottom-1 w-full h-9 bg-amber-300 rounded -z-10 mask-t-from-0.5"></span></span>
                         </h3>
                         <p className='text-lg text-green-400'>
                             No TemperoBR, cada garfada é uma viagem pelos sabores do nosso Brasil.
                             Da feijoada caprichada ao tropeiro da vovó, entregamos comida com alma, feita com carinho e aquele tempero que aquece o coração.
                         </p>
-                        <button className="px-4 py-2 border border-green-400 text-green-400 rounded-full text-sm hover:bg-green-100 transition cursor-pointer">Ver Cardápio</button>
+                        <button className="px-4 py-2 hover:bg-green-400 hover:text-white hover:border-green-400 transition-all duration-300 border-2 rounded-4xl cursor-pointer">Ver Cardápio</button>
                     </div>
                 </div>
             </div>
