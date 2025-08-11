5338bbf642f0c7603e45f04041b692f6739765ac
larissa <rissapins@gmail.com>
2025-08-10 22:02:24 -0300

    refactor: ajusta o componente Home e remove o arquivo Home e a pasta Pages
    update Home component layout and remove old Home page file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Cardapio from "./components/cardapio/Cardapio"
-import Contato from "./components/contato/Contato"
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar"
+import Cardapio from "./components/cardapio/Cardapio";
+import Contato from "./components/contato/Contato";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col gap-24 mt-24 pt-[88px]">
+      <main className="flex flex-col gap-24 pt-[88px]">
         <Home />
         <Cardapio />
         <Contato />
